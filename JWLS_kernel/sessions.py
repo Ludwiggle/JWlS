787d4b680d4dfc7a3830f7c65441ea6efb7334ba
Ben Mares <15216687+maresb@users.noreply.github.com>
2019-11-18 23:38:29 +0100

    Include JWLS.wl with kernel resources so that it no longer requires installation

--- JWLS_kernel/sessions.py
+++ JWLS_kernel/sessions.py
@@ -1,7 +1,16 @@
 import tempfile
 import pathlib
 import os
+import stat
 import pexpect
+
+# https://stackoverflow.com/a/20885799/10155767
+try:
+    import importlib.resources as pkg_resources
+except ImportError:
+    # Try backported to PY<37 `importlib_resources`.
+    import importlib_resources as pkg_resources
+
 
 class FifoWolframscriptSession:
 
@@ -11,13 +20,21 @@ class FifoWolframscriptSession:
 
         os.mkfifo(self.temp_path / "wlin.fifo")
         (self.temp_path / "wlout.txt").touch()
+        write_script_to(self.temp_path / "JWLS.wl")
 
         p = str(self.temp_path)
         cmd = f'bash -c "tail -f {p}/wlin.fifo | ' + \
-            f'JWLS.wl --tmpdir={p}"'
+            f'{p}/JWLS.wl --tmpdir={p}"'
         print(cmd)
         self.child = pexpect.spawn(cmd)
 
     def close(self):
         self.child.sendintr()
         self.temporary_directory.cleanup()
+
+
+def write_script_to(file_name):
+    script = pkg_resources.read_binary(__package__, 'JWLS.wl')
+    with open(file_name, "wb") as out_file:
+        out_file.write(script)
+    os.chmod(file_name, os.stat(file_name).st_mode | stat.S_IEXEC)
